c8ef2783813879a0d3746f3a96f7ec3ef1f72fc9
Gregory Veloso <gveloso@labcetec.net>
2025-04-15 21:40:12 -0300

    Adicionando via api, as funções de get e add funcionario

--- quarto/src/hooks/useApi.js
+++ quarto/src/hooks/useApi.js
@@ -1,2 +1,50 @@
-const url = import.meta.env.VITE_API_URL
+// Importação do useState para criar as variáveis
+import { useState, useEffect } from "react";
 
+// Variável para url da api, vindo do arquivo .env
+ const url = import.meta.env.VITE_API_URL;
+//const url = "http://localhost:5000/funcionarios";
+
+//Função para requisitar todos os funcionários
+export function getFuncionarios() {
+  const [funcionarios, setFuncionarios] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+        try{
+           //Faz a requisição pra api, com a url designada
+           const response = await fetch(url)
+           //Converte a resposta da api pra json
+           const data = await response.json()
+           setFuncionarios(data)
+           console.log("Dados recebidos:", data)
+        }
+        catch(error){
+            console.log("Erro ao buscar os dados:", error)
+        }
+    }
+    fetchData()
+  }, []);
+  
+  //Retorna a lista de funcionários, já com os dados da API
+  return funcionarios;
+}
+
+
+export function addFuncionario(funcionario){
+    async function fetchData() {
+        try{
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(funcionario)
+            })
+            const data = await response.json()
+            console.log("Usuário adicionado:", data)
+        }
+        catch(error){
+            console.log("Erro ao cadastrar funcionário:", error)
+        }
+    }
+    fetchData()
+}
